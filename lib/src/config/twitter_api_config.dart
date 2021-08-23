cc8c11c9d359f7803c140bc3cc9126472bc40f85
Ajinkya Karanjikar <37205552+AjinkyaASK@users.noreply.github.com>
2021-08-23 08:54:39 +0530

    This is accidentally pushed to be on version control

--- lib/src/config/twitter_api_config.dart
+++
@@ -1,7 +0,0 @@
-class TwitterApiConfig {
-  static const String apiKey = 'AnQh2PzZXFz7DW4fDXMKJglc0';
-  static const String apiSecret =
-      'lBoZnNHFQollS1xEN2hwyKYuoKELb8RceznWtMd6rB5iVSDehz';
-  static const String redirectUrl =
-      'https://the-places-app.firebaseapp.com/__/auth/handler';
-}
